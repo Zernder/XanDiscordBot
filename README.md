bf284958789ea66f5d28c8a773db2e30888d5d3e
Xanmal <kmmf88@gmail.com>
2023-12-27 12:54:20 -0700

    Fresh Start

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# Xans Python Discord Bot
- A discord bot I programmed in Python
+# XanDiscordBot
+The Most Basic Discord bot, no chatting just does basic commands for moderation and plays music added to a "Music" folder.
+
+
+Steps to listen to your own music:
+
+1. Create a 'Music' folder, any .mp3 or .wav file you put in there will be playable with the music cog by doing /play.
+
+add the following lines to config.py:
+
+Token = "Token Here" 
+Replace Token Here with the discordbot token you got from the Discord dev portal.
+
+Dev Portal: https://discord.com/developers/applications
+
+After that, simple run the "BotRun.bat" File.
---
+++ XanDiscordBot.py
@@ -0,0 +1,101 @@
+# TamaBot.py Script
+
+# ======================[ IMPORTS AND INITIALIZATIONS ]======================
+import os
+import warnings
+import tracemalloc
+import discord
+from discord.ext import commands
+import asyncio
+from config import BotToken
+import requests
+
+
+
+tracemalloc.start()
+
+# ======================[ GLOBAL VARIABLES ]================================
+user_message_logs = {}
+
+Token = BotToken
+
+# ======================[ BOT SETUP ]======================
+
+# Define the bot
+client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+client.chatlog_dir = "logs/"
+
+
+
+# Inside your code, before the warning occurs, you can suppress it like this:
+warnings.filterwarnings("ignore", category=UserWarning)
+
+
+# ======================[ EVENT HANDLERS ]======================
+
+
+## ---- On Ready Event ----
+
+@client.event
+async def on_ready():
+    pass
+
+## ---- On Message Event ----
+
+@client.event
+async def on_message(message):
+    # Check if the message starts with '!'
+    if message.content.startswith('!'):
+        await client.process_commands(message)  # Process the command
+
+
+
+
+
+# ======================[ Load and Reload COGS]======================
+
+@client.command()
+async def reload(ctx, extension):
+    extension_name = f'Cogs.{extension}'
+    # First try to unload the extension if it's already loaded
+    if extension_name in client.extensions:
+        try:
+            await client.unload_extension(extension_name)
+        except Exception as e:
+            await ctx.send(f'Could not unload the extension {extension}. Error: {e}')
+            return  # Stop the command if unloading fails
+    else:
+        await ctx.send(f'The extension {extension} is not loaded, attempting to load...')
+ # Now try to load the extension
+    try:
+        await client.load_extension(extension_name)
+        await ctx.send(f'Reloaded {extension} successfully!')
+    except Exception as e:
+        # Send a different message if it's now being loaded for the first time
+        if extension_name not in client.extensions:
+            await ctx.send(f'Loaded {extension} successfully!')
+        else:
+            await ctx.send(f'Could not load the extension {extension}. Error: {e}')
+
+async def load():
+    for filename in os.listdir('./Cogs'):
+        if filename.endswith('.py'):
+            await client.load_extension(f'Cogs.{filename[:-3]}')
+        else:
+            print(f'Unable to load {filename[:-3]}')
+
+# =====================
+# Main Async Function
+# =====================
+
+async def main():
+    # This function will be called from the main.py, so it should be an async function
+    # and should not start the event loop itself.
+    await load()  # This loads any necessary components, e.g., Cogs.
+    print("Bot Online!")
+    await client.start(Token)
+
+# The following block allows the script to run independently.
+if __name__ == "__main__":
+    asyncio.run(main())
+
